c15914e454e55603d3ce6406d4e842005bbe38fd
Aurora Haro Vicente <aurora.havi@gmail.com>
2024-11-10 21:06:30 +0100

    Creo el constructor en el enum Direccion.

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Direccion.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Direccion.java
@@ -4,6 +4,6 @@ public enum Direccion {
     NORESTE ("Noreste"), SURESTE ("Sureste"), SUROESTE ("Suroeste"), NOROESTE("Noroeste");
 
     private String cadenaAMostrar;
-    }
+    private Direccion(String mensajeAMostrar) {this.cadenaAMostrar=mensajeAMostrar;}
 }
 
