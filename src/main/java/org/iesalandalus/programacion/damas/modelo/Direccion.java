ef9e17dcee6b4b6367319a38ebc3775657990da7
Aurora Haro Vicente <aurora.havi@gmail.com>
2024-11-10 21:02:48 +0100

    Creo el atributo cadenaAMostrar (String).

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Direccion.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Direccion.java
@@ -3,14 +3,7 @@ package org.iesalandalus.programacion.damas.modelo;
 public enum Direccion {
     NORESTE ("Noreste"), SURESTE ("Sureste"), SUROESTE ("Suroeste"), NOROESTE("Noroeste");
 
-    private String mensajeAMostrar;
-    private Direccion(String mensajeAMostrar)
-    {
-        this.mensajeAMostrar=mensajeAMostrar;
-    }
-
-    @Override
-    public String toString() {
-        return ordinal()+".-"+mensajeAMostrar;
+    private String cadenaAMostrar;
     }
 }
+
