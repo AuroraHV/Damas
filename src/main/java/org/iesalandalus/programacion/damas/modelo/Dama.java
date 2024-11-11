96c7b967c1c539bf8a9ee1e602bc309f2415c372
Aurora Haro Vicente <aurora.havi@gmail.com>
2024-11-11 16:09:43 +0100

    Creo la clase Dama con sus atributos.

---
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Dama.java
@@ -0,0 +1,8 @@
+package org.iesalandalus.programacion.damas.modelo;
+
+public class Dama {
+    //Atributos
+    private Color color;
+    private Posicion posicion;
+    private boolean esDamaEspecial;
+}
