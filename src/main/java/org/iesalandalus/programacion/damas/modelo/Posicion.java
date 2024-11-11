9a5fb97a64d4787cd280aff46b88b970400d7d7b
Aurora Haro Vicente <aurora.havi@gmail.com>
2024-11-11 02:03:04 +0100

    Creo los métodos get y set para Posición.

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Posicion.java
@@ -4,4 +4,28 @@ public class Posicion {
 
     private int fila;
     private char columna;
+
+    public int getFila() {
+        return fila;
+    }
+    public void setFila(int fila) {
+        if (fila < 1 || fila > 8) {
+            throw new IllegalArgumentException("ERROR: Sólo hay filas entre la 1 y la 8.");
+        }
+        this.fila = fila;
+    }
+
+    public char getColumna() {
+        return columna;
+    }
+    public void setColumna(char columna) {
+        if (columna < 'a' || columna > 'h') {
+            throw new IllegalArgumentException("ERROR: Sólo hay columnas entre la a y la h.");
+        }
+        this.columna = columna;
+    }
+
+
+
+
 }
