41c59a993ae9832cccb28bb47e8772091a551ff9
Aurora Haro Vicente <aurora.havi@gmail.com>
2024-11-11 16:40:28 +0100

    Creo los métodos get y set en Dama con sus excepciones.

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Dama.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Dama.java
@@ -1,8 +1,36 @@
 package org.iesalandalus.programacion.damas.modelo;
 
 public class Dama {
-    //Atributos
+    //1-Atributos
     private Color color;
     private Posicion posicion;
     private boolean esDamaEspecial;
+
+    //2.1-Métodos get y set con la excepción de color
+    public Color getColor() {
+        return color;
+    }
+    public void setColor(Color color) {
+        if (color == null) {
+            throw new NullPointerException("El color no puede ser nulo.");
+        }
+        this.color = color;
+    }
+    //2.2-Métodos get y set con la excepción de posicion
+    public Posicion getPosicion() {
+        return posicion;
+    }
+    public void setPosicion(Posicion posicion) {
+        if (posicion == null) {
+            throw new NullPointerException("La posicion no puede ser nula.");
+        }
+        this.posicion = posicion;
+    }
+    //2.3-Métodos get y set con la excepción de esDamaEspecial.
+    public boolean isEsDamaEspecial() {
+        return esDamaEspecial;
+    }
+    public void setEsDamaEspecial(boolean esDamaEspecial) {
+        this.esDamaEspecial = esDamaEspecial;
+    }
 }
