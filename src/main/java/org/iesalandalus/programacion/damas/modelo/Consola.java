10ff0f51b8b5624c91a3a8c3cb63fe0701910966
Aurora Haro Vicente <aurora.havi@gmail.com>
2024-11-13 02:10:16 +0100

    Creo el constructor de consola.

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Consola.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Consola.java
@@ -2,6 +2,11 @@ package org.iesalandalus.programacion.damas.modelo;
 
 public class Consola {
 
+    // Constructor privado para evitar la creación de instancias ya que sólo contendrá métodos estáticos
+    private Consola() {
+    }
+
+
 
 
 }
