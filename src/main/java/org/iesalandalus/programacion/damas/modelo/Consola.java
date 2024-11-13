aee22a15f2dc4c36395ddc725db512b1038953bd
Aurora Haro Vicente <aurora.havi@gmail.com>
2024-11-13 02:13:29 +0100

    Creo el método para mostrar las opciones de la consola.

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Consola.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Consola.java
@@ -5,6 +5,14 @@ public class Consola {
     // Constructor privado para evitar la creación de instancias ya que sólo contendrá métodos estáticos
     private Consola() {
     }
+    //Menú para las opciones de la aplicación
+    public static void mostrarMenu() {
+        System.out.println("Menú de opciones:");
+        System.out.println("1. Crear dama por defecto");
+        System.out.println("2. Crear dama eligiendo el color");
+        System.out.println("3. Mover dama");
+        System.out.println("4. Salir");
+    }
 
 
 
